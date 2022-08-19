61f62d0c1ab708446cef922568c8a03a18e07f21
Anel Ibarra <ajdevops26@gmail.com>
2022-08-18 20:31:07 -0500

    carrito de compras, mejoras a los menus em mobile y desktop con javascript

--- main.js
+++ main.js
@@ -2,16 +2,44 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu =  document.querySelector('.desktop-menu');
 const mobileMenu =  document.querySelector('.mobile-menu');
 const burgerMenu =  document.querySelector('.menu');
+const cartIcon =  document.querySelector('.navbar-shopping-cart ');
+const shoppingCart =  document.querySelector('.product-detail');
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu );
 
     function toggleDesktopMenu(){
+        const isShoppingCartclosed = shoppingCart.classList.contains('inactive');
+        if(!isShoppingCartclosed){
+            shoppingCart.classList.add('inactive');
+            
+        } 
+        mobileMenu.classList.toggle('inactive');
+    
     desktopMenu.classList.toggle('inactive');
     }
 
-burgerMenu.addEventListener('click', toggleMobilepMenu );
+burgerMenu.addEventListener('click', toggleMobileMenu );
 
-    function toggleMobilepMenu(){
+    function toggleMobileMenu(){
+        const isShoppingCartclosed = shoppingCart.classList.contains('inactive');
+        if(!isShoppingCartclosed){
+            shoppingCart.classList.add('inactive');
+        } 
         mobileMenu.classList.toggle('inactive');
     }
+cartIcon.addEventListener('click', toggleShoppingCart );
+
+    function toggleShoppingCart(){
+        const isMobilemenuclosed = mobileMenu.classList.contains('inactive'); 
+        const isDesktopmenuclosed = desktopMenu.classList.contains('inactive'); 
+        
+        if(!isMobilemenuclosed){
+            mobileMenu.classList.add('inactive');
+        }
+        if(!isDesktopmenuclosed){
+            desktopMenu.classList.add('inactive');
+        }
+        shoppingCart.classList.toggle('inactive');
+    }
+
